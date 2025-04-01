33e224aee25b943ef27b8ae9bc57f3e699521877
jenson-bolton-ac <115472244+jenson-bolton-ac@users.noreply.github.com>
2025-04-01 11:43:44 +0100

    Initial Commit

---
+++ SRC/Asteroids.cpp
@@ -0,0 +1,219 @@
+#include "Asteroid.h"
+#include "Asteroids.h"
+#include "Animation.h"
+#include "AnimationManager.h"
+#include "GameUtil.h"
+#include "GameWindow.h"
+#include "GameWorld.h"
+#include "GameDisplay.h"
+#include "Spaceship.h"
+#include "BoundingShape.h"
+#include "BoundingSphere.h"
+#include "GUILabel.h"
+
+// PUBLIC INSTANCE CONSTRUCTORS ///////////////////////////////////////////////
+
+/** Constructor. Takes arguments from command line, just in case. */
+Asteroids::Asteroids(int argc, char *argv[])
+	: GameSession(argc, argv)
+{
+	mLevel = 0;
+	mAsteroidCount = 0;
+}
+
+/** Destructor. */
+Asteroids::~Asteroids(void)
+{
+}
+
+// PUBLIC INSTANCE METHODS ////////////////////////////////////////////////////
+
+/** Start an asteroids game. */
+void Asteroids::Start()
+{
+	// Create a shared pointer for the Asteroids game object - DO NOT REMOVE
+	shared_ptr<Asteroids> thisPtr = shared_ptr<Asteroids>(this);
+
+	// Add this class as a listener of the game world
+	mGameWorld->AddListener(thisPtr.get());
+
+	// Add this as a listener to the world and the keyboard
+	mGameWindow->AddKeyboardListener(thisPtr);
+
+	// Add a score keeper to the game world
+	mGameWorld->AddListener(&mScoreKeeper);
+
+	// Add this class as a listener of the score keeper
+	mScoreKeeper.AddListener(thisPtr);
+
+	// Create a spaceship and add it to the world
+	mGameWorld->AddObject(CreateSpaceship());
+	// Create some asteroids and add them to the world
+	CreateAsteroids(10);
+
+	//Create the GUI
+	CreateGUI();
+
+	// Add a player (watcher) to the game world
+	mGameWorld->AddListener(&mPlayer);
+
+	// Add this class as a listener of the player
+	mPlayer.AddListener(thisPtr);
+
+	// Start the game
+	GameSession::Start();
+}
+
+/** Stop the current game. */
+void Asteroids::Stop()
+{
+	// Stop the game
+	GameSession::Stop();
+}
+
+// PUBLIC INSTANCE METHODS IMPLEMENTING IKeyboardListener /////////////////////
+
+void Asteroids::OnKeyPressed(uchar key, int x, int y)
+{
+	switch (key)
+	{
+	case ' ':
+		mSpaceship->Shoot();
+		break;
+	default:
+		break;
+	}
+}
+
+void Asteroids::OnKeyReleased(uchar key, int x, int y) {}
+
+void Asteroids::OnSpecialKeyPressed(int key, int x, int y)
+{
+	switch (key)
+	{
+	// If up arrow key is pressed start applying forward thrust
+	case GLUT_KEY_UP: mSpaceship->Thrust(10); break;
+	// If left arrow key is pressed start rotating anti-clockwise
+	case GLUT_KEY_LEFT: mSpaceship->Rotate(90); break;
+	// If right arrow key is pressed start rotating clockwise
+	case GLUT_KEY_RIGHT: mSpaceship->Rotate(-90); break;
+	// Default case - do nothing
+	default: break;
+	}
+}
+
+void Asteroids::OnSpecialKeyReleased(int key, int x, int y)
+{
+	switch (key)
+	{
+	// If up arrow key is released stop applying forward thrust
+	case GLUT_KEY_UP: mSpaceship->Thrust(0); break;
+	// If left arrow key is released stop rotating
+	case GLUT_KEY_LEFT: mSpaceship->Rotate(0); break;
+	// If right arrow key is released stop rotating
+	case GLUT_KEY_RIGHT: mSpaceship->Rotate(0); break;
+	// Default case - do nothing
+	default: break;
+	} 
+}
+
+
+// PUBLIC INSTANCE METHODS IMPLEMENTING IGameWorldListener ////////////////////
+
+void Asteroids::OnObjectRemoved(GameWorld* world, shared_ptr<GameObject> object)
+{
+}
+
+// PUBLIC INSTANCE METHODS IMPLEMENTING ITimerListener ////////////////////////
+
+void Asteroids::OnTimer(int value)
+{
+	if (value == CREATE_NEW_PLAYER)
+	{
+		mSpaceship->Reset();
+		mGameWorld->AddObject(mSpaceship);
+	}
+
+	if (value == START_NEXT_LEVEL)
+	{
+		mLevel++;
+		int num_asteroids = 10 + 2 * mLevel;
+	}
+
+}
+
+// PROTECTED INSTANCE METHODS /////////////////////////////////////////////////
+shared_ptr<GameObject> Asteroids::CreateSpaceship()
+{
+	// Create a raw pointer to a spaceship that can be converted to
+	// shared_ptrs of different types because GameWorld implements IRefCount
+	mSpaceship = make_shared<Spaceship>();
+	mSpaceship->SetBoundingShape(make_shared<BoundingSphere>(mSpaceship->GetThisPtr(), 4.0f));
+	shared_ptr<Shape> spaceship_shape = make_shared<Shape>("spaceship.shape");
+	shared_ptr<Shape> thruster_shape = make_shared<Shape>("thruster.shape");
+	shared_ptr<Shape> bullet_shape = make_shared<Shape>("bullet.shape");
+	mSpaceship->SetSpaceshipShape(spaceship_shape);
+	mSpaceship->SetThrusterShape(thruster_shape);
+	mSpaceship->SetBulletShape(bullet_shape);
+
+	// Reset spaceship back to centre of the world
+	mSpaceship->Reset();
+	// Return the spaceship so it can be added to the world
+	return mSpaceship;
+}
+
+void Asteroids::CreateAsteroids(const uint num_asteroids)
+{
+	shared_ptr<Shape> asteroid_shape = make_shared<Shape>("asteroid.shape");
+	for (uint i = 0; i < num_asteroids; i++) {
+		shared_ptr<GameObject> asteroid = make_shared<Asteroid>();
+		asteroid->SetBoundingShape(make_shared<BoundingSphere>(asteroid->GetThisPtr(), 10.0f));
+		asteroid->SetShape(asteroid_shape);
+		mGameWorld->AddObject(asteroid);
+	}
+}
+
+void Asteroids::CreateGUI()
+{
+	// Add a (transparent) border around the edge of the game display
+	mGameDisplay->GetContainer()->SetBorder(GLVector2i(10, 10));
+	// Create a new GUILabel and wrap it up in a shared_ptr
+	mScoreLabel = make_shared<GUILabel>("Score: 0");
+	// Set the vertical alignment of the label to GUI_VALIGN_TOP
+	mScoreLabel->SetVerticalAlignment(GUIComponent::GUI_VALIGN_TOP);
+	// Add the GUILabel to the GUIComponent  
+	shared_ptr<GUIComponent> score_component
+		= static_pointer_cast<GUIComponent>(mScoreLabel);
+	mGameDisplay->GetContainer()->AddComponent(score_component, GLVector2f(0.0f, 1.0f));
+
+	// Create a new GUILabel and wrap it up in a shared_ptr
+	mLivesLabel = make_shared<GUILabel>("Lives: 3");
+	// Set the vertical alignment of the label to GUI_VALIGN_BOTTOM
+	mLivesLabel->SetVerticalAlignment(GUIComponent::GUI_VALIGN_BOTTOM);
+	// Add the GUILabel to the GUIComponent  
+	shared_ptr<GUIComponent> lives_component = static_pointer_cast<GUIComponent>(mLivesLabel);
+	mGameDisplay->GetContainer()->AddComponent(lives_component, GLVector2f(0.0f, 0.0f));
+}
+
+void Asteroids::OnScoreChanged(int score)
+{
+	// Format the score message using an string-based stream
+	std::ostringstream msg_stream;
+	msg_stream << "Score: " << score;
+	// Get the score message as a string
+	std::string score_msg = msg_stream.str();
+	mScoreLabel->SetText(score_msg);
+}
+
+void Asteroids::OnPlayerKilled(int lives_left)
+{
+	// Format the lives left message using an string-based stream
+	std::ostringstream msg_stream;
+	msg_stream << "Lives: " << lives_left;
+	// Get the lives left message as a string
+	std::string lives_msg = msg_stream.str();
+	mLivesLabel->SetText(lives_msg);
+}
+
+
+
